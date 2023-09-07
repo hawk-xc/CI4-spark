5141898936393552a2cee6da6fc7f8369e0aea38
wahyu tri <hawkxc068@gmail.com>
2023-09-07 16:51:24 +0700

    add semantic.ui element

--- project-21/app/Views/dashboard/index.php
+++ project-21/app/Views/dashboard/index.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/dashboardStyle.css">
     <link rel="icon" href="assets/icon/icon-dark.png" sizes="20x20">
+    <link rel="stylesheet" href="semantic/semantic.min.css">
+    <script src="semantic/semantic.min.js"></script>
     <title>dashboard</title>
 </head>
 
@@ -14,14 +16,23 @@
         <div class="sidebar">
             <img src="assets/icon/icon-name.png" alt="" style="width: 150px;">
             <div class="panel">
-                <span>dashboard</span>
-                <span>ticket</span>
-                <span>statistic</span>
+                <button class="ui primary button">Dashboard</button>
+                <button class="ui button">Ticket</button>
+                <button class="ui button">Statistic</button>
             </div>
         </div>
-        <div class="content">
-            <h2>Content</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor vero nostrum tempora distinctio corporis!</p>
+        <div class="ui container">
+            <div class="content-header">
+                <div class="content-header-title">
+                    <h2 class="ui header">Content</h2>
+                </div>
+                <div class="content-header-menu">
+                    <div class="ui action input">
+                        <input type="text" placeholder="Search...">
+                        <button class="ui button">Search</button>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </body>
