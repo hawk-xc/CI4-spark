215c5e0644e2743471463e7bbb19a74c24133382
wahyu tri <hawkxc068@gmail.com>
2023-09-08 06:19:19 +0700

    maintain styling, and add semantic ui framework

--- project-21/app/Views/dashboard/index.php
+++ project-21/app/Views/dashboard/index.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="css/dashboardStyle.css">
     <link rel="icon" href="assets/icon/icon-dark.png" sizes="20x20">
     <link rel="stylesheet" href="semantic/semantic.min.css">
+    <link rel="stylesheet" href="semantic/all.min.css">
     <script src="semantic/semantic.min.js"></script>
     <title>dashboard</title>
 </head>
@@ -19,17 +20,41 @@
                 <button class="ui primary button">Dashboard</button>
                 <button class="ui button">Ticket</button>
                 <button class="ui button">Statistic</button>
+                <button class="ui button">My Logs</button>
             </div>
         </div>
         <div class="ui container">
             <div class="content-header">
                 <div class="content-header-title">
-                    <h2 class="ui header">Content</h2>
+                    <h2 class="ui header"># Fresh Dashboard</h2>
                 </div>
                 <div class="content-header-menu">
                     <div class="ui action input">
                         <input type="text" placeholder="Search...">
                         <button class="ui button">Search</button>
+                        <a class="ui label">
+                            <img class="ui right spaced avatar image" src="assets/users/guest-default-man.png">
+                            Guest
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="content-body">
+                <div class="panel-info">
+                    <div class="info-box">
+                        <h3 class="ui white inverted header">
+                            New Installation
+                        </h3>
+                    </div>
+                    <div class="info-box">
+                        <h3 class="ui white inverted header">
+                            New Installation
+                        </h3>
+                    </div>
+                    <div class="info-box">
+                        <h3 class="ui white inverted header">
+                            New Installation
+                        </h3>
                     </div>
                 </div>
             </div>
