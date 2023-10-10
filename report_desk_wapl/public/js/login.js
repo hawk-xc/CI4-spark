9ba851540bda7cffceadc9d2fe13505eab0f440f
wahyutc <hawkxc068@gmail.com>
2023-10-11 01:44:15 +0700

    menyelesaikan login page #login

---
+++ report_desk_wapl/public/js/login.js
@@ -0,0 +1,20 @@
+const passwd_field = document.getElementById("password_field");
+const passwd_toggle = document.getElementById("show_pass_toggle");
+
+passwd_field.addEventListener("input", function () {
+  if (passwd_field.value == "") {
+    passwd_toggle.classList.add("hidden");
+  } else {
+    passwd_toggle.classList.remove("hidden");
+  }
+});
+
+passwd_toggle.addEventListener("click", function () {
+  if (passwd_toggle.classList.contains("ri-eye-line")) {
+    passwd_toggle.classList.replace("ri-eye-line", "ri-eye-off-line");
+    passwd_field.setAttribute("type", "text");
+  } else {
+    passwd_toggle.classList.replace("ri-eye-off-line", "ri-eye-line");
+    passwd_field.setAttribute("type", "password");
+  }
+});
