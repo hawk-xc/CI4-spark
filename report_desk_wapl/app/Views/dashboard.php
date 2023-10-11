0b5f2cbbd2a815b19d7d363fd18b11159b6447cd
Dewan22blue-hawk <dennyirawan170204@gmail.com>
2023-10-11 17:49:39 +0700

    changes dashboard content

--- report_desk_wapl/app/Views/dashboard.php
+++ report_desk_wapl/app/Views/dashboard.php
@@ -10,7 +10,9 @@
 <body>
     <?= $this->extend('particle/dashboard_particle.php'); ?>
     <?= $this->section('content'); ?>
-    <h1 class="text-2xl font-extrabold">testing</h1>
+    <h1 class="text-2xl font-extrabold text-center">testing</h1>
+    <h1 class="text-2xl font-extrabold text-center">ora ngono anjeng</h1>
+
     <?= $this->endSection(); ?>
 </body>
 
