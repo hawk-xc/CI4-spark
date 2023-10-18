48fd895dc2efed0125757fc6c204c6ec30ef7883
wahyutc <hawkxc068@gmail.com>
2023-10-19 02:03:43 +0700

    add migration

--- report_desk_wapl/app/Database/Migrations/2023-10-18-183749_Ticket.php
+++ report_desk_wapl/app/Database/Migrations/2023-10-18-183749_Ticket.php
@@ -8,11 +8,37 @@ class Ticket extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 10,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'subject' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+            ],
+            'type' => [
+                'type' => "ENUM('new', 'mt')",
+                'default' => 'new',
+            ],
+            'contact_id' => [
+                'type' => 'INT',
+                'constraint' => 255,
+            ],
+            'description' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255
+            ],
+        ]);
+
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('tickets');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('tickets');
     }
 }
