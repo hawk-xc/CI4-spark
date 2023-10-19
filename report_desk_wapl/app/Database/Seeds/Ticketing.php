825b722a6a5eb273f1f8561e227beb30f3fa9de5
wahyutc <hawkxc068@gmail.com>
2023-10-19 15:44:26 +0700

    add seeder and migration

---
+++ report_desk_wapl/app/Database/Seeds/Ticketing.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class Ticketing extends Seeder
+{
+    public function run()
+    {
+        $data = [
+            'subject' => 'NEW INSTALLATION',
+            'type' => 'new',
+            'contact_id' => 1,
+            'description' => 'REPORT FROM CUSTOMER AT 08.22 12 September 2023'
+        ];
+
+        $this->db->query("INSERT INTO tickets (subject, type, contact_id, description) VALUES (:subject:, :type:, :contact_id:, :description:)", $data);
+
+        // $this->db->table('tickets')->insert($data);
+    }
+}
--- report_desk_wapl/app/Models/TicketModel.php
+++ report_desk_wapl/app/Models/TicketModel.php
@@ -13,7 +13,7 @@ class TicketModel extends Model
     protected $returnType       = 'array';
     protected $useSoftDeletes   = false;
     // protected $protectFields    = true;
-    // protected $allowedFields    = [];
+    protected $allowedFields    = ['subject', 'type', 'contact_id', 'description'];
 
     // Dates
     protected $useTimestamps = true;
