c7fe336c23e547c7dbd6b1378ddf38dd8c205897
Dewan22blue-hawk <dennyirawan170204@gmail.com>
2023-11-05 16:53:15 +0700

    menambahkan form terbaru

--- report_desk_wapl/public/js/contactDropdown.js
+++ report_desk_wapl/public/js/contactDropdown.js
@@ -1,38 +1,35 @@
 let openDropdown = null;
 
 document.addEventListener("DOMContentLoaded", function () {
-  // Ini buat perulangan nggawe menu Id yang berbeda ben gak podo
-
   for (let i = 1; i <= 1000000; i++) {
     const menuButton = document.getElementById("menu-button" + i);
     const menu = document.getElementById("menu-button-menu" + i);
 
-    if (menuButton && id) {
-    }
-    menuButton.addEventListener("click", function (event) {
-      if (openDropdown !== menu) {
-        // hide dropdown
+    if (menuButton && menu) {
+      menuButton.addEventListener("click", function (event) {
+        if (openDropdown !== menu) {
+          if (openDropdown) {
+            openDropdown.classList.add("hidden");
+          }
+          event.stopPropagation();
+          menu.classList.remove("hidden");
+          openDropdown = menu;
+        } else {
+          menu.classList.add("hidden");
+          openDropdown = null;
+        }
+      });
+
+      document.addEventListener("click", function () {
         if (openDropdown) {
           openDropdown.classList.add("hidden");
+          openDropdown = null;
         }
+      });
+
+      menu.addEventListener("click", function (event) {
         event.stopPropagation();
-        menu.classList.remove("hidden");
-        openDropdown = menu;
-      } else {
-        menu.classList.add("hidden");
-        openDropdown = null;
-      }
-    });
-
-    document.addEventListener("click", function () {
-      if (openDropdown) {
-        openDropdown.classList.add("hidden"); // Menghilangkan list menu ketika di klik di luar button
-        openDropdown = null;
-      }
-    });
-
-    menu.addEventListener("click", function (event) {
-      event.stopPropagation(); //  Mencegah hide ketika meng-klik didalam menu
-    });
+      });
+    }
   }
 });
