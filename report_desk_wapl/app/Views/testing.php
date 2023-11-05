39d2dd55a8f6e792986c9f67e4f2fa3a9c34904a
wahyutc <hawkxc068@gmail.com>
2023-11-05 09:46:03 +0700

    add update

--- report_desk_wapl/app/Views/testing.php
+++ report_desk_wapl/app/Views/testing.php
@@ -1,6 +1,8 @@
 <?= $this->extend('./particle/dashboardParticle.php'); ?>
 <?= $this->section('content'); ?>
-<div class="flex bg-blue-100 p-10 opacity-0">
-    <span>hello</span>
+<div class="flex bg-blue-100 p-10 ">
+    <span>
+        <?= $homeNavButton = true ?>
+    </span>
 </div>
 <?= $this->endSection() ?>
