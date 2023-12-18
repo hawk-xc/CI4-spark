bb43567589fc3f48b71ca8b069c645776c0b5b28
Dewan22blue-hawk <dennyirawan170204@gmail.com>
2023-12-18 10:59:01 +0700

    edited dropdown contact

--- report_desk_wapl/public/js/contactDropdown.js
+++ report_desk_wapl/public/js/contactDropdown.js
@@ -4,8 +4,9 @@ document.addEventListener("DOMContentLoaded", function () {
   for (let i = 1; i <= 1000000; i++) {
     const menuButton = document.getElementById("menu-button" + i);
     const menu = document.getElementById("menu-button-menu" + i);
+    const icon = document.getElementById("dropDownIcon" + i);
 
-    if (menuButton && menu) {
+    if (menuButton && menu && icon) {
       menuButton.addEventListener("click", function (event) {
         if (openDropdown !== menu) {
           if (openDropdown) {
@@ -13,9 +14,11 @@ document.addEventListener("DOMContentLoaded", function () {
           }
           event.stopPropagation();
           menu.classList.remove("hidden");
+          icon.classList.add("rotate-180");
           openDropdown = menu;
         } else {
           menu.classList.add("hidden");
+          icon.classList.remove("rotate-180");
           openDropdown = null;
         }
       });
@@ -23,6 +26,7 @@ document.addEventListener("DOMContentLoaded", function () {
       document.addEventListener("click", function () {
         if (openDropdown) {
           openDropdown.classList.add("hidden");
+          icon.classList.remove("rotate-180");
           openDropdown = null;
         }
       });
